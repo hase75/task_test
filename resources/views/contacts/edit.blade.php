ed3e7a8058eef4c0e6fc37208161410b79666b78
Keigo <122374696+hase75@users.noreply.github.com>
2023-10-28 15:53:53 +0900

    [Finish]111. update 更新画面

--- resources/views/contacts/edit.blade.php
+++ resources/views/contacts/edit.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <section class="text-gray-600 body-font relative">
-                    <form method="post" action="{{ route('contacts.store') }}">
+                    <form method="post" action="{{ route('contacts.update', ['id' => $contact->id]) }}">
                         @csrf
 
                         <div class="container px-5 mx-auto">
@@ -70,7 +70,7 @@
                             <div class="p-2 w-full">
                             <div class="relative">
                                 <label for="contact" class="leading-7 text-sm text-gray-600">お問い合わせ内容</label>
-                                <textarea id="contact" name="contact" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">{{ $contact->contact }}"</textarea>
+                                <textarea id="contact" name="contact" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">{{ $contact->contact }}</textarea>
                             </div>
                             </div>
 
