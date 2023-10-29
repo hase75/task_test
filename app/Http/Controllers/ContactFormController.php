d6cacc7bf591edc65d264a0e71b000294e329f8f
Keigo <122374696+hase75@users.noreply.github.com>
2023-10-29 13:14:56 +0900

    [Finish]114. バリデーション(フォームリクエスト))

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\ContactForm;
 use App\Services\CheckFormService;
+use App\Http\Requests\StoreContactRequest;
+use App\Http\Requests\UpdateContactRequest;
 
 class ContactFormController extends Controller
 {
@@ -36,7 +38,7 @@ class ContactFormController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreContactRequest $request)
     {
         // dd($request, $request->name);
 
@@ -91,7 +93,7 @@ class ContactFormController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UpdateContactRequest $request, $id)
     {
         $contact = ContactForm::find($id);
         $contact->name = $request->name;
--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -9,16 +9,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
+                    <x-input-error class="mb-4" :messages="$errors->all()"/>
                     <section class="text-gray-600 body-font relative">
                     <form method="post" action="{{ route('contacts.store') }}">
                         @csrf
 
-
-
-
-
                         <div class="container px-5 mx-auto">
-
                         <div class="lg:w-1/2 md:w-2/3 mx-auto">
                         <div class="flex flex-wrap -m-2">
                             <div class="p-2 w-full">
@@ -42,14 +38,12 @@
                             </div>
                             </div>
 
-
                             <div class="p-2 w-full">
                             <div class="relative">
                                 <label for="url" class="leading-7 text-sm text-gray-600">ホームページ</label>
                                 <input type="url" id="url" name="url" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                             </div>
                             </div>
-
 
                             <div class="p-2 w-full">
                             <div class="relative">
@@ -59,23 +53,20 @@
                             </div>
                             </div>
 
-
                             <div class="p-2 w-full">
-                                <div class="relative">
-                                    <label for="age" class="leading-7 text-sm text-gray-600">年齢</label>
-                                    <select name="age">
-                                        <option value="">選択してください</option>
-                                        <option value="1">〜19歳</option>
-                                        <option value="2">20歳〜29歳</option>
-                                        <option value="3">30歳〜39歳</option>
-                                        <option value="4">40歳〜49歳</option>
-                                        <option value="5">50歳〜59歳</option>
-                                        <option value="6">60歳〜</option>
-                                    </select>
-                                </div>
-                                </div>
-
-
+                            <div class="relative">
+                                <label for="age" class="leading-7 text-sm text-gray-600">年齢</label>
+                                <select name="age">
+                                    <option value="">選択してください</option>
+                                    <option value="1">〜19歳</option>
+                                    <option value="2">20歳〜29歳</option>
+                                    <option value="3">30歳〜39歳</option>
+                                    <option value="4">40歳〜49歳</option>
+                                    <option value="5">50歳〜59歳</option>
+                                    <option value="6">60歳〜</option>
+                                </select>
+                            </div>
+                            </div>
 
                             <div class="p-2 w-full">
                             <div class="relative">
@@ -84,24 +75,18 @@
                             </div>
                             </div>
 
-
                             <div class="p-2 w-full">
                             <div class="relative">
                                 <input type="checkbox" id="caution" name="caution">注意事項に同意する
                             </div>
                             </div>
 
-
                             <div class="p-2 w-full">
                             <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>
                             </div>
-
-
-
-
                         </div>
                         </div>
-                    </div>
+                        </div>
                     </form>
                     </section>
                 </div>
